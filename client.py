6a0d93700b8c913e1e59f6d2004b4fd785a86be6
Meli-0xFF <martin.melichercik@gmail.com>
2020-05-04 23:03:17 +0200

    Method distance4 implemented, complete_neighbourhood fixed.
    Signed-off-by: Meli-0xFF <martin.melichercik@gmail.com>

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import asyncio
 
 LOCAL_HOST = 'http://localhost:'
 
+
 class Node():
     def __init__(self, port: str):
         self.port = port
@@ -33,7 +34,7 @@ async def make_edge(v_1: str, v_2: str):
 async def complete_neighbourhood(start):
     root = await get_node(start)
     for n in [await get_node(n) for n in root.neighbours]:
-        for v in ((root.neighbours | set([root.port])) - n.neighbours - set([n.port])):
+        for v in (root.neighbours - n.neighbours - set([n.port]) - set([root.port])):
             await make_edge(n.port, v)
 
 async def climb_degree(start):
@@ -44,8 +45,28 @@ async def climb_degree(start):
     else : return await climb_degree(degrees.get(max(degrees)))
 
 
+async def bfs(start, used: dict, dist: int):
+    root = await get_node(start)
+
+    if used.get(root.port) is None: used[root.port] = dist
+
+    if dist < 4:
+        for node in root.neighbours:
+            if used.get(node) is None: used[node] = used[root.port]+1
+    else: return
+
+    for node in root.neighbours:
+        await bfs(node, used, dist+1)
+
+
+async def distance4(start):
+    used_nodes = {}
+    await bfs(start, used_nodes, 0)
+    return set(map(lambda a: a[0], set(filter(lambda x: x[1] == 4, used_nodes.items()))))
+
+
 async def main():
-    print(await climb_degree('8034'))
+    print(await distance4('8034'))
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
